ad9674859a300772fe867cb6a8c73df6e992b815
Steve Smith <stevejsmith567@gmail.com>
2017-03-21 12:06:54 +0000

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -95,8 +95,8 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotin('Buy peacock feathers', page_text)
-        self.assertNotin('make a fly', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
         
         # Francis starts a new list by entering a new item.
         # He is less interesting than Edith..
